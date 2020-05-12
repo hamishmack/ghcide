97606ff0155e9f3028cedbcf5ea54b2a34ee349a
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2020-05-12 16:06:01 +1200

    Support newer hie-bios

--- exe/Main.hs
+++ exe/Main.hs
@@ -389,7 +389,7 @@ memoIO op = do
             Just res -> return (mp, res)
 
 setOptions :: GhcMonad m => ComponentOptions -> DynFlags -> m (DynFlags, [Target])
-setOptions (ComponentOptions theOpts _) dflags = do
+setOptions (ComponentOptions { componentOptions = theOpts }) dflags = do
     cacheDir <- liftIO $ getCacheDir theOpts
     (dflags', targets) <- addCmdOpts theOpts dflags
     let dflags'' =
